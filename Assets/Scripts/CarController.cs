e8d9b9368e6f973fef391724fc71a77dd179bedd
MaxoN <m.niechaiev@gmail.com>
2024-04-02 23:33:36 +0300

    imported vehicle. Wheel colliders, move forward

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] WheelCollider[] wheels;
+    [SerializeField] private float torque;
+    
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            for (var i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = torque;
+            }
+        }
+        else
+        {
+            for (var i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = 0;
+            }
+        }
+    }
+}
