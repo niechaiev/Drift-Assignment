c3eedf103ce9572513302504ec58511f14e78f1a
MaxoN <m.niechaiev@gmail.com>
2024-04-17 15:36:12 +0300

    Remove MobileResolver. API = 33. Firebase Analytics

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,14 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+}
